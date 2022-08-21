11cd9fe4dec259c5c0616371a6f33703f0d3b66d
Jacob Janak <jakeathotmail@gmail.com>
2022-08-21 14:46:22 -0400

    Added rate limiting and error handling

--- app.js
+++ app.js
@@ -3,27 +3,55 @@ require('dotenv').config()
 const express = require('express');
 const axios = require('axios');
 
-const app = express();
+let nextRequestTime = 0;
+let lastPrice = 0;
 
+const app = express();
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.sendFile('index.html')
-})
+const currentTime = () => Math.floor(new Date().getTime() / 1000);
+
+const nextMonthStartTime = () => {
+  const date = new Date();
+  date.setMonth(date.getMonth() + 1);
+  date.setDate(1);
+  date.setHours(0);
+  date.setMinutes(0);
+  date.setSeconds(0);
+  date.setMilliseconds(0);
+  return Math.floor(date.getTime() / 1000);
+};
+
+const updateRateLimit = requestsLeft => {
+  if (requestsLeft === 0) {
+    nextRequestTime = nextMonthStartTime();
+  } else {
+    const secondsLeft = nextMonthStartTime() - currentTime();
+    const rate = Math.ceil(secondsLeft / requestsLeft);
+    nextRequestTime = currentTime() + rate;
+  }
+};
+
+app.get('/', (req, res) => res.sendFile('index.html'))
 
 app.get('/price', (req, res) => {
-  axios.get('https://api.coinranking.com/v2/coin/Qwsogvtv82FCd/price', {
-    params: {
-      'x-access-token:': process.env.API_KEY
-    }
-  })
-  .then(response => {
-    const price = response.data.data.price;
-    res.json(price)
-  })
-  .catch(error => {
-    res.json(error);
-  })
+  if (currentTime() >= nextRequestTime) {
+    axios.get('https://api.coinranking.com/v2/coin/Qwsogvtv82FCd/price', {
+      params: { 'x-access-token:': process.env.API_KEY }
+    })
+    .then(response => {
+      updateRateLimit(response.headers['x-ratelimit-remaining-month']);
+      const price = response.data.data.price;
+      lastPrice = price;
+      res.json({ success: true, price: price })
+    })
+    .catch(error => {
+      const status = error.response.status === 429 ? 429 : 500;
+      res.json({ success: false, status: status })
+    })
+  } else {
+    res.json({ success: true, price: lastPrice })
+  }
 })
 
 app.listen(3000)
--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,32 @@
-const loadPrice = () => {
-	$.get('/price', price => {
-		console.log(price)
-		price = (Math.round(price * 100) / 100).toFixed(2);
 
-		if (!isNan(price)) {
-			document.title = price;
-			$('.price-display').text(price)
+const showError = errorMessage => {
+	$('#error-message').text(errorMessage)
+	$('.alert').removeClass('hide')
+}
+
+const hideError = () => $('.alert').addClass('hide');
+
+const loadPrice = () => {
+	$.get('/price', response => {
+		if (response.success) {
+			const price = (Math.round(response.price * 100) / 100).toFixed(2);
+			if (!isNaN(price) && price > 0) {
+				document.title = price;
+				$('#price-display').text(price)
+				hideError()
+			} else {
+				showError('Server error')
+			}
+		} else {
+			if (response.status === 429) showError('API error')
+			else showError('Server error')
 		}
 	})
+	.fail(() => showError(`Can't connect to server`))
 }
 
 loadPrice()
 
 window.setInterval(loadPrice, 5000)
+
+$('.btn-close').on('click', () => hideError())
